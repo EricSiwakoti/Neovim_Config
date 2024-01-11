cbba4fecdc9b2066cfce4cfbbdf630ecab1a4dcc
Eric <bonderic44@gmail.com>
2024-01-11 10:08:26 +0545

    chore : made minor changes

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,67 +1,92 @@
-local config = function()
-	require("nvim-treesitter.configs").setup({
-		build = ":TSUpdate",
-		indent = {
-			enable = true,
-		},
-		autotag = {
-			enable = true,
-		},
-		event = {
-			"BufReadPre",
-			"BufNewFile",
-		},
-		ensure_installed = {
-			"rust",
-			"markdown",
-      "bash",
-			"json",
-			"javascript",
-			"typescript",
-			"yaml",
-      "prisma",
-      "go",
-			"html",
-      "http",
-			"css",
-			"markdown",
-			"bash",
-      "json",
-			"lua",
-			"dockerfile",
-			"solidity",
-      "regex",
-      "tsx",
-      "terraform",
-      "ssh_config",
-      "sql",
-      "scss",
-			"gitignore",
-			"python",
-			"vue",
-      "vim",
-			"svelte",
-			"toml",
-		},
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = true,
-		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<C-s>",
-				node_incremental = "<C-s>",
-				scope_incremental = false,
-				node_decremental = "<BS>",
-			},
-		},
-	})
-end
-
 return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	config = config,
+    "nvim-treesitter/nvim-treesitter",
+    event = "BufReadPre",
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })
+    end or ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+        "nvim-treesitter/playground",
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        { "nvim-treesitter/nvim-treesitter-context", config = true },
+    },
+
+   config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "bash",
+                "c",
+                "cpp",
+                "css",
+                "dockerfile",
+                "go",
+                "graphql",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "prisma",
+                "python",
+                "scss",
+                "svelte",
+                "sql",
+                "terraform",
+                "tsx",
+                "typescript",
+                "vue",
+                "yaml",
+                "solidity",
+                "toml",
+            },
+            sync_install = false,
+            auto_install = true,
+            autotag = {
+                enable = true,
+            },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ["aa"] = "@parameter.outer",
+                        ["ia"] = "@parameter.inner",
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                    },
+                },
+            },
+            highlight = {
+                enable = true,
+                extended_mode = true,
+                disable = function(_, buf)
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
+                additional_vim_regex_highlighting = false,
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<c-space>",
+                    node_incremental = "<c-space>",
+                    scope_incremental = "<c-s>",
+                    node_decremental = "<c-backspace>",
+                },
+            },
+            playground = {
+                enable = true,
+            },
+            query_linter = {
+                enable = true,
+                use_virtual_text = true,
+                lint_events = { "BufWrite", "CursorHold" },
+            },
+        })
+    end,
 }
