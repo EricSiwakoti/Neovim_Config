2f53b012751ed75893b22b1f3939b3e2c53682e7
Eric <bonderic44@gmail.com>
2024-01-11 10:00:46 +0545

    chore : made minor changes

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,14 @@
-return {
-	{ "folke/neoconf.nvim", cmd = "Neoconf" },
-	"folke/neodev.nvim",
+return{
+   {
+    { "folke/neoconf.nvim", cmd = "Neoconf" },
+       "folke/neodev.nvim",
+   },
+   {
+   'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+    require('home.dashboard')
+    end,
+    dependencies = { {'nvim-tree/nvim-web-devicons'}}
+   }
 }
