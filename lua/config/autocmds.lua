d1da0b257ad2a6a75511b6b350c14a1a79fef96e
Eric <bonderic44@gmail.com>
2024-01-25 13:37:14 +0545

    feat : autocommands for nvim tree and trimming whitespace and blanklines added

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,4 +1,7 @@
--- Auto-format on save
+local autocmd, command, cmd, fn =
+  vim.api.nvim_create_autocmd, vim.api.nvim_create_user_command, vim.cmd, vim.fn
+
+-- Auto format on save
 local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
 	group = lsp_fmt_group,
@@ -9,7 +12,7 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 			return
 		end
 
-		vim.lsp.buf.format({ name = "efm", async = true })
+		vim.lsp.buf.format()
 	end,
 })
 
@@ -20,4 +23,52 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
 		vim.highlight.on_yank()
 	end,
+})
+
+-- Automatically close NvimTree if it's the last buffer on window
+local nvimtree_close_group = vim.api.nvim_create_augroup("NvimTreeCloseGroup", {})
+vim.api.nvim_create_autocmd('QuitPre', {
+	group = nvimtree_close_group,
+   	callback = function()
+    local invalid_win = {}
+    local wins = vim.api.nvim_list_wins()
+    for _, w in ipairs(wins) do
+      local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+      if bufname:match('NvimTree_') ~= nil then table.insert(invalid_win, w) end
+    end
+    if #invalid_win == #wins - 1 then
+      -- Should quit, so we close all invalid windows.
+      for _, w in ipairs(invalid_win) do
+        vim.api.nvim_win_close(w, true)
+      end
+    end
+  end,
+})
+
+-- Trim trailing whitespace and trailing blank lines on save
+local function trim_trailing_whitespace()
+  local pos = vim.api.nvim_win_get_cursor(0)
+  cmd([[silent keepjumps keeppatterns %s/\s\+$//e]])
+  vim.api.nvim_win_set_cursor(0, pos)
+end
+command('TrimWhitespace', trim_trailing_whitespace, {})
+
+local function trim_trailing_lines()
+  local last_line = vim.api.nvim_buf_line_count(0)
+  local last_nonblank_line = vim.fn.prevnonblank(last_line)
+  if last_nonblank_line < last_line then vim.api.nvim_buf_set_lines(0, last_nonblank_line, last_line, true, {}) end
+end
+command('TrimTrailingLines', trim_trailing_lines, {})
+
+local function trim()
+  if not vim.o.binary and vim.o.filetype ~= 'diff' then
+    trim_trailing_lines()
+    trim_trailing_whitespace()
+  end
+end
+
+local trim_on_save_group = vim.api.nvim_create_augroup("TrimOnSaveGroup", {})
+autocmd('BufWritePre', {
+  group = trim_on_save_group,
+  callback = trim,
 })
