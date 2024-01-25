fc8f7944def3dfb832db9eda3efccc2f172c04bb
Eric <bonderic44@gmail.com>
2024-01-25 13:40:10 +0545

    feat : time display at statusline

--- lua/plugins/lualine-nvim.lua
+++ lua/plugins/lualine-nvim.lua
@@ -1,24 +1,25 @@
 local config = function()
-	require("lualine").setup({
-		options = {
-			theme = "auto",
-			globalstatus = true,
-			component_separators = { left = "|", right = "|" },
-			section_separators = { left = "", right = "" },
-		},
-		sections = {
-			lualine_a = { "mode" },
-			lualine_b = { "buffers" },
-			lualine_x = { "encoding", "fileformat", "filetype" },
-			lualine_y = { "progress" },
-			lualine_z = { "location" },
-		},
-		tabline = {},
-	})
+    require("lualine").setup({
+        options = {
+            theme = "auto",
+            globalstatus = true,
+            component_separators = { left = "|", right = "|" },
+            section_separators = { left = "", right = "" },
+        },
+        sections = {
+            lualine_a = { "mode" },
+            lualine_b = { "buffers" },
+            lualine_x = { "encoding", "fileformat", "filetype" },
+            lualine_y = { "progress" },
+            lualine_z = { "location", function() return os.date("%H:%M:%S") end },
+        },
+        tabline = {},
+        extensions = { "fugitive", "nvim-tree" },
+    })
 end
 
 return {
-	"nvim-lualine/lualine.nvim",
-	lazy = false,
-	config = config,
+    "nvim-lualine/lualine.nvim",
+    lazy = false,
+    config = config
 }
