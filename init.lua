e624db981f8e70823e190a3a4f39db80f27b94f2
Eric <bonderic44@gmail.com>
2024-01-25 13:35:13 +0545

    feat : default terminal powershell added

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
 require("config")
 vim.o.history = 0
 vim.o.shell = 'pwsh'
+
+-- Open terminal in a bottom horizontal split
+vim.api.nvim_set_keymap('n', '<leader>tm', ':split | terminal<CR>', { noremap = true, silent = true })
+
+-- Resize the terminal window
+vim.cmd([[
+  autocmd TermOpen * setlocal nonumber norelativenumber signcolumn=no
+  autocmd TermOpen * resize 12
+]])
