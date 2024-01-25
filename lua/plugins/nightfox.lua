26aa640bec1344b15a804a74e13decba69fd7288
Eric <bonderic44@gmail.com>
2024-01-25 15:33:23 +0545

    feat : transparency added

--- lua/plugins/nightfox.lua
+++ lua/plugins/nightfox.lua
@@ -3,6 +3,18 @@ return {
 	lazy = false,
 	priority = 999,
 	config = function ()
-		vim.cmd('colorscheme carbonfox')
+		require('nightfox').setup({
+				  options = {
+					transparent = true,
+    				terminal_colors = true,
+    				styles = {
+      					comments = "italic",
+      					keywords = "bold",
+						functions = "italic,bold",
+      					types = "italic,bold",
+   					}
+ 				}
+		})
+		vim.cmd("colorscheme carbonfox")
 	end
 }
