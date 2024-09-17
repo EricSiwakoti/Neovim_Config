21e1bcf708a08a4f8a472848b0f0031f636adb6e
Eric <bonderic44@gmail.com>
2024-09-17 21:45:33 +0545

    feat : Add PowerShell configuration and improve terminal key mappings

--- init.lua
+++ init.lua
@@ -1,11 +1,14 @@
 require("config")
 vim.o.history = 0
-vim.o.shell = 'pwsh'
+vim.o.shell = "pwsh"
+vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+vim.o.shellquote = ""
+vim.o.shellxquote = ""
 
--- Open terminal in a bottom horizontal split
-vim.api.nvim_set_keymap('n', '<leader>tm', ':split | terminal<CR>', { noremap = true, silent = true })
-
--- Resize the terminal window
+vim.api.nvim_set_keymap('n', '<F8>', ':split |terminal<CR>', {noremap = true, silent = true})
 vim.cmd([[
   autocmd TermOpen * setlocal nonumber norelativenumber signcolumn=no
+  autocmd TermOpen * startinsert
 ]])
