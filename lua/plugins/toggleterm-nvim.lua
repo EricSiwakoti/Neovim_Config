aef56116999367cb870641ef2ca4715220f8acaf
Eric <bonderic44@gmail.com>
2024-09-17 21:50:25 +0545

    feat : Enhance toggleterm setup and added pwsh as shell

--- lua/plugins/toggleterm-nvim.lua
+++ lua/plugins/toggleterm-nvim.lua
@@ -1,20 +1,31 @@
 return {
   'akinsho/toggleterm.nvim',
   version = "*",
+  event = 'BufReadPre',
+  lazy = false,
   config = function()
     require("toggleterm").setup({
         size = 10,
         open_mapping = [[<F7>]],
+        shade_filetypes = {},
+        shade_terminals = true,
         shading_factor = 2,
+        start_in_insert = true,
+        close_on_exit = false,
+        persist_size = true,
         direction = "float",
         float_opts = {
             border = "curved",
-            highlights = {
-                border = "Normal",
-                background = "Normal",
-            },
+            winblend = 0,
         },
-    }) 
-
+        highlights = {
+          border = "Normal",
+          background = "Normal",
+          FloatBorder = {
+            guifg = "#208397"
+          }
+        },
+        shell = "pwsh", 
+    })
   end,
 }
