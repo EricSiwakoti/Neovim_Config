809bf334506fbadf84caae0f74cf7396407c8051
Eric <bonderic44@gmail.com>
2024-09-22 17:13:02 +0545

    feat : Add automatic installation and separate tools configuration

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,30 +1,59 @@
-local opts = {
-  	-- Whatever language/tech we write inside this block, it will automatically get installed
-	ensure_installed = {
-		"efm",
-		"bashls",
-		"ts_ls",
-		"solidity",
-		"tailwindcss",
-		"pyright",
-		"emmet_ls",
-		"jsonls",
-    	"clangd",
-    	"dockerls",
-		"gopls", 
-	},
+local lsp_opts = {
+    ensure_installed = {
+        'bashls',  
+        'clangd',
+        'dockerls', 
+        'efm',
+        'emmet_ls',  
+        'gopls',
+        'jsonls',  
+        'pyright',
+        'solidity',  
+        'tailwindcss', 
+        'ts_ls',  
+    },
+    automatic_installation = true,
+}
 
-	automatic_installation = true,
+local tool_opts = {
+    ensure_installed = {
+        'black',
+        'clang-format',
+        'cpplint',
+        'eslint_d',
+        'fixjson',
+        'flake8',
+        'goimports',
+        'golangci-lint',
+        'hadolint',
+        'prettier',
+        'shellcheck',
+        'shfmt',
+        'solhint',
+    },
+    automatic_installation = true,
 }
 
 return {
-	"williamboman/mason-lspconfig.nvim",
-	opts = opts,
-	event = "BufReadPre",
-	lazy = false,
-	dependencies = "williamboman/mason.nvim",
-	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup(opts)
-	end,
+    {
+        "williamboman/mason-lspconfig.nvim",
+        opts = lsp_opts,
+        event = "BufReadPre",
+        lazy = false,
+        dependencies = "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup(lsp_opts)
+        end,
+    },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        opts = tool_opts,
+        event = "BufReadPre",
+        lazy = false,
+        dependencies = "williamboman/mason.nvim",
+        config = function()
+            require("mason-tool-installer").setup(tool_opts)
+        end,
+    },
 }
