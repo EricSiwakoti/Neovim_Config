ba0e450303caf4c73d1955ad34d7c88f67d06809
Eric <bonderic44@gmail.com>
2024-09-22 17:19:48 +0545

    refactor : Enhance LSP configuration with Mason integration, improved diagnostics, and expanded tool support

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,65 +2,124 @@ local on_attach = require("util.lsp").on_attach
 local diagnostic_signs = require("util.icons").diagnostic_signs
 
 local config = function()
-	require("neoconf").setup({})
-	local cmp_nvim_lsp = require("cmp_nvim_lsp")
-	local lspconfig = require("lspconfig")
-	local capabilities = cmp_nvim_lsp.default_capabilities()
+    require("neoconf").setup({})
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local lspconfig = require("lspconfig")
+    local mason_lspconfig = require("mason-lspconfig")
+    local mason_registry = require("mason-registry")
+    local capabilities = cmp_nvim_lsp.default_capabilities()
 
-	for type, icon in pairs(diagnostic_signs) do
-		local hl = "DiagnosticSign" .. type
-		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-	end
+    for type, icon in pairs(diagnostic_signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
 
-	-- Json
-	lspconfig.jsonls.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		filetypes = { "json", "jsonc" },
-	})
+    vim.diagnostic.config({
+        virtual_text = true,
+        update_in_insert = true,
+        underline = true,
+        severity_sort = { reverse = false },
+        float = {
+            focusable = true,
+            style = "minimal",
+            border = "rounded",
+            source = true,
+            header = "",
+            prefix = "",
+        },
+    })
 
-	-- Python
-	lspconfig.pyright.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		settings = {
-			pyright = {
-				disableOrganizeImports = false,
-				analysis = {
-					useLibraryCodeForTypes = true,
-					autoSearchPaths = true,
-					diagnosticMode = "workspace",
-					autoImportCompletions = true,
-				},
-			},
-		},
-	})
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+        signs = true,
+        underline = true,
+        virtual_text = {
+            spacing = 5,
+            severity = { min = vim.diagnostic.severity.HINT },
+        },
+        update_in_insert = true,
+    })
 
-	-- Typescript
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+        vim.lsp.handlers.hover,
+        {
+            border = "rounded"  -- Options: "single", "double", "rounded", "solid", "shadow"
+        }
+    )
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+        vim.lsp.handlers.signature_help,
+        {
+            border = "rounded"  -- Options: "single", "double", "rounded", "solid", "shadow"
+        }
+    )
+
+    vim.keymap.set("n", "dn", vim.diagnostic.goto_next)
+    vim.keymap.set("n", "dN", vim.diagnostic.goto_prev)
+    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open diagnostic float" })
+
+    vim.keymap.set("n", "<leader>lh", function()
+        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ 0 }))
+    end, { desc = "Toggle inlay hints" })
+
+    vim.cmd([[autocmd FileType * set formatoptions-=ro]])
+
+    -- Json
+    lspconfig.jsonls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = { "json", "jsonc" },
+    })
+
+    -- Python
+    lspconfig.pyright.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = { "python" },
+        root_dir = lspconfig.util.root_pattern("pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile"),
+        settings = {
+            pyright = {
+                disableOrganizeImports = false,
+                analysis = {
+                    useLibraryCodeForTypes = true,
+                    autoSearchPaths = true,
+                    diagnosticMode = "workspace",
+                    autoImportCompletions = true,
+                    typeCheckingMode = "basic"
+                },
+                executionEnvironments = {
+                    { root = ".", extraPaths = { "src", "lib", "tests" } }
+                },
+                reportMissingImports = "none",
+                reportMissingModuleSource = "none"
+            }
+        },
+    })
+
+    -- Typescript
 	lspconfig.ts_ls.setup({
 		on_attach = on_attach,
 		capabilities = capabilities,
-		filetypes = {
-			"typescript",
-		},
-		root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git"),
+		filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+        root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
 	})
 
-	-- Bash
-	lspconfig.bashls.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		filetypes = { "sh", "aliasrc" },
-	})
+    -- Bash
+    lspconfig.bashls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = { "sh", "aliasrc" },
+        root_dir = lspconfig.util.root_pattern(".git", ".bashrc", ".zshrc", ".bash_profile", ".bash_aliases", ".bash_history", ".bash_logout", ".bashrc", ".bash_login", ".bash_logout", ".profile", ".zshrc", ".zshenv", ".zprofile", ".zlogin", ".zlogout"),
+    })
 
-	-- Solidity
-	lspconfig.solidity.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		filetypes = { "solidity" },
-	})
+    -- Solidity
+    lspconfig.solidity.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = { "solidity" },
+        root_dir = lspconfig.util.root_pattern("truffle-config.js", "hardhat.config.js", "package.json"),
+    })
 
-	-- Typescriptreact, Javascriptreact, Css, Sass, Scss, Less, Svelte, Vue
+    -- Emmet
 	lspconfig.emmet_ls.setup({
 		capabilities = capabilities,
 		on_attach = on_attach,
@@ -78,107 +137,197 @@ local config = function()
 		},
 	})
 
-	-- Docker
-	lspconfig.dockerls.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-	})
+    -- Docker
+    lspconfig.dockerls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = { "dockerfile" },
+        root_dir = lspconfig.util.root_pattern("Dockerfile", ".git"),
+    })
 
-	-- C/C++
-	lspconfig.clangd.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		cmd = {
-			"clangd",
-			"--offset-encoding=utf-16",
-		},
-	})
+    -- C/C++
+    lspconfig.clangd.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        cmd = {
+            "clangd",
+            "--offset-encoding=utf-16",
+        },
+        filetypes = { "c", "cpp", "objc", "objcpp" },
+    })
 
-	-- Go
-	lspconfig.gopls.setup({
-   		 capabilities = capabilities,
-   		 on_attach = on_attach,
-	})
+    -- Go
+    lspconfig.gopls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = { "go", "gomod" },
+        root_dir = lspconfig.util.root_pattern("go.mod", ".git"),
+    })
 
-	local flake8 = require("efmls-configs.linters.flake8")
-	local black = require("efmls-configs.formatters.black")
-	local eslint_d = require("efmls-configs.linters.eslint_d")
-	local prettierd = require("efmls-configs.formatters.prettier_d")
-	local fixjson = require("efmls-configs.formatters.fixjson")
-	local shellcheck = require("efmls-configs.linters.shellcheck")
-	local shfmt = require("efmls-configs.formatters.shfmt")
-	local hadolint = require("efmls-configs.linters.hadolint")
-	local solhint = require("efmls-configs.linters.solhint")
-	local cpplint = require("efmls-configs.linters.cpplint")
-	local clang_format = require("efmls-configs.formatters.clang_format")
-	local goimports = require("efmls-configs.formatters.goimports")
-	local golangcilint = require("efmls-configs.linters.golangci_lint")
+    -- TailwindCSS
+    lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+        root_dir = lspconfig.util.root_pattern("tailwind.config.js", "tailwind.config.cjs", "tailwind.config.ts", "tailwind.config.tsx"),
+    })
 
-	-- Configure efm server
-	lspconfig.efm.setup({
-		filetypes = {
-			"python",
-			"json",
-			"jsonc",
-			"sh",
-			"javascript",
-			"javascriptreact",
-			"typescript",
-			"typescriptreact",
-			"svelte",
-			"vue",
-			"markdown",
-			"docker",
-			"solidity",
-			"html",
-			"css",
-			"c",
-			"cpp",
-			"go",
-		},
-		init_options = {
-			documentFormatting = true,
-			documentRangeFormatting = true,
-			hover = true,
-			documentSymbol = true,
-			codeAction = true,
-			completion = true,
-		},
-		settings = {
-			languages = {
-				python = { flake8, black },
-				typescript = { eslint_d, prettierd },
-				json = { eslint_d, fixjson },
-				jsonc = { eslint_d, fixjson },
-				sh = { shellcheck, shfmt },
-				javascript = { eslint_d, prettierd },
-				javascriptreact = { eslint_d, prettierd },
-				typescriptreact = { eslint_d, prettierd },
-				svelte = { eslint_d, prettierd },
-				vue = { eslint_d, prettierd },
-				markdown = { prettierd },
-				docker = { hadolint, prettierd },
-				solidity = { solhint },
-				html = { prettierd },
-				css = { prettierd },
-				c = { clang_format, cpplint },
-				cpp = { clang_format, cpplint },
-				go = { goimports, golangcilint },
-			},
-		},
-	})
+    local function get_tool_config(tool_name)
+        if tool_name == 'prettier' then
+            return {
+                formatCommand = "prettier --stdin-filepath ${INPUT}",
+                formatStdin = true,
+            }
+        elseif tool_name == 'flake8' then
+            return {
+                lintCommand = "flake8 --stdin-display-name ${INPUT} -",
+                lintStdin = true,
+                lintFormats = { "%f:%l:%c: %m" },
+            }
+        elseif tool_name == 'eslint_d' then
+            return {
+                lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
+                lintStdin = true,
+                lintFormats = { "%f:%l:%c: %m" },
+            }
+        elseif tool_name == 'shellcheck' then
+            return {
+                lintCommand = "shellcheck -f gcc -x",
+                lintFormats = { "%f:%l:%c: %m" },
+            }
+        elseif tool_name == 'hadolint' then
+            return {
+                lintCommand = "hadolint -f gcc",
+                lintFormats = { "%f:%l:%c: %m" },
+            }
+        elseif tool_name == 'solhint' then
+            return {
+                lintCommand = "solhint -f unix",
+                lintFormats = { "%f:%l:%c: %m" },
+            }
+        elseif tool_name == 'cpplint' then
+            return {
+                lintCommand = "cpplint",
+                lintFormats = { "%f:%l: %m" },
+            }
+        elseif tool_name == 'golangci-lint' then
+            return {
+                lintCommand = "golangci-lint run --out-format=line-number",
+                lintFormats = { "%f:%l:%c: %m" },
+            }
+        elseif tool_name == 'black' then
+            return {
+                formatCommand = "black --quiet -",
+                formatStdin = true,
+            }
+        elseif tool_name == 'fixjson' then
+            return {
+                formatCommand = "fixjson",
+                formatStdin = true,
+            }
+        elseif tool_name == 'shfmt' then
+            return {
+                formatCommand = "shfmt -",
+                formatStdin = true,
+            }
+        elseif tool_name == 'clang-format' then
+            return {
+                formatCommand = "clang-format -assume-filename=${INPUT}",
+                formatStdin = true,
+            }
+        elseif tool_name == 'goimports' then
+            return {
+                formatCommand = "goimports",
+                formatStdin = true,
+            }
+        else
+            print(string.format("Error: Unable to find config for %s", tool_name))
+            return nil
+        end
+    end
+
+    local function get_tool(category, tool_name)
+        local config = get_tool_config(tool_name)
+        if config then
+            return config
+        end
+
+        if mason_registry.is_installed(tool_name) then
+            return mason_registry.get_package(tool_name)
+        else
+            print(string.format("Error: Unable to find %s in category %s", tool_name, category))
+            return nil
+        end
+    end
+
+    local flake8 = get_tool('lint', 'flake8')
+    local eslint_d = get_tool('lint', 'eslint_d')
+    local shellcheck = get_tool('lint', 'shellcheck')
+    local hadolint = get_tool('lint', 'hadolint')
+    local solhint = get_tool('lint', 'solhint')
+    local cpplint = get_tool('lint', 'cpplint')
+    local golangci_lint = get_tool('lint', 'golangci-lint')
+    local black = get_tool('format', 'black')
+    local fixjson = get_tool('format', 'fixjson')
+    local shfmt = get_tool('format', 'shfmt')
+    local clang_format = get_tool('format', 'clang-format')
+    local goimports = get_tool('format', 'goimports')
+    local prettier = get_tool('format', 'prettier')
+
+    local languages = {
+        python = { flake8, black },
+        typescript = { eslint_d, prettier },
+        json = { eslint_d, fixjson },
+        jsonc = { eslint_d, fixjson },
+        sh = { shellcheck, shfmt },
+        javascript = { eslint_d, prettier },
+        javascriptreact = { eslint_d, prettier },
+        typescriptreact = { eslint_d, prettier },
+        svelte = { eslint_d, prettier },
+        vue = { eslint_d, prettier },
+        markdown = { prettier },
+        docker = { hadolint, prettier },
+        solidity = { solhint },
+        html = { prettier },
+        css = { prettier },
+        c = { clang_format, cpplint },
+        cpp = { clang_format, cpplint },
+        go = { goimports, golangci_lint },
+    }
+
+    -- Configure efm server
+    local efm_config = {
+        filetypes = vim.tbl_keys(languages),
+        settings = {
+            rootMarkers = { '.git/' },
+            languages = languages,
+        },
+        init_options = {
+            documentFormatting = true,
+            documentRangeFormatting = true,
+            hover = true,
+            documentSymbol = true,
+            codeAction = true,
+            completion = true,
+        },
+    }
+
+    lspconfig.efm.setup(vim.tbl_extend('force', efm_config, {
+        capabilities = capabilities,
+        on_attach = on_attach,
+    }))
 end
 
 return {
-	"neovim/nvim-lspconfig",
-	config = config,
-	lazy = false,
-	dependencies = {
-		"windwp/nvim-autopairs",
-		"williamboman/mason.nvim",
-		"creativenull/efmls-configs-nvim",
-		"hrsh7th/nvim-cmp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-nvim-lsp",
-	},
+    "neovim/nvim-lspconfig",
+    config = config,
+    lazy = false,
+    dependencies = {
+        "windwp/nvim-autopairs",
+        "williamboman/mason.nvim",
+        "creativenull/efmls-configs-nvim",
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-nvim-lsp",
+    },
 }
